2fab545c3d1a3ee7ba6c1f58edea0bcb9f470db6
PurpleSeer <ilya.khristolyubov@yandex.ru>
2020-12-18 01:36:31 +0500

    1.0

---
+++ Lab5/Lab5.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "PL.h"
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	Cat* Cat1 = new Cat();
+	My_cat* My_cat1 = new My_cat();
+	Cat* Cat2 = new My_cat();
+
+	cout << "\nПроверка на принадлежность\n";
+	cout << "\nС помощью метода Classname:\n";
+	cout << Cat1->ClassName() << endl;
+	cout << My_cat1->ClassName() << endl;
+	cout << Cat2->ClassName() << endl;
+
+	cout << "\nС помощью метода isA:\n";
+	cout << Cat1->isA("Cat") << endl;
+	cout << My_cat1->isA("My_cat") << endl;
+	cout << Cat2->isA("Cat") << endl << endl;
+
+	cout << "Проверка типов dynamic_cast:\n";
+
+	My_cat* My_cat2 = dynamic_cast<My_cat*>(Cat2);
+	cout << My_cat2->ClassName() << endl;
+	My_cat2->Method();
+	Cat2->Method();
+	cout << endl;
+
+	My_cat* My_cat3 = new My_cat;
+	cout << "\nОпасное приведение типов:\n";
+	if (Cat2->isA("Pear_Tree"))
+		My_cat3 = (My_cat*)Cat2;
+	cout << My_cat3->ClassName() << endl;
+
+	return 0;
+}
